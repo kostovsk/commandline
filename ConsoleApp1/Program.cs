5636b244a0e21dc946b4b8bd8b692d71b0e12985
Stoycho Kostov <stoichokostov@gmail.com>
2021-08-10 20:56:57 -0500

    quick test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommandLine;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v',"verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        Console.WriteLine($"Verbose output enabled. Current arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+        }
+    }
+}
